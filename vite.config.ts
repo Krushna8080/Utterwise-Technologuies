b3679c6061454d7ed31fb4803f745ec207a1d3a7
Krushna8080 <krushnadandge8080@gmail.com>
2025-02-05 14:54:26 +0530

    ft commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../dist',
+  },
+  server: {
+    host: '0.0.0.0', // 🔥 Expose to Render
+    port: 10000, // ✅ Render automatically assigns a port, so we'll use process.env.PORT
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
